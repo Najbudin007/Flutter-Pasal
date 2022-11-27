ce8158f04cf9a125df2fb7eb1d0f55340a155f19
najbudin007 <najbudinalam55@gmail.com>
2022-11-27 08:52:55 +0545

    all chnages til now

---
+++ lib/presentation/cart/components/cart_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CartCard extends StatelessWidget {
+  const CartCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 88,
+          child: AspectRatio(
+            aspectRatio: 0.88,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: const Color(0xFFF5F6F9),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Image.asset(),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              cart.product.title,
+              style: const TextStyle(color: Colors.black, fontSize: 16),
+              maxLines: 2,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
